9fc7d07e10c44200cf6e93384475de6df322b5c8
Amit Kulkarni <kulkarni.am@husky.neu.edu>
2017-02-09 19:07:38 -0500

    Added new header, image, youtube widget code

--- public/assignment/services/widget.service.client.js
+++ public/assignment/services/widget.service.client.js
@@ -10,7 +10,7 @@
             {_id: "345", widgetType : "IMAGE", pageId: "321", width:"90%", url : "https://s-media-cache-ak0.pinimg.com/originals/a2/2a/0a/a22a0a7e624943303b23cc326598c167.jpg"},
             {_id: "456", widgetType : "HTML", pageId: "123", text: "<p>Some text of paragraph</p>"},
             {_id: "567", widgetType : "HEADER", pageId: "321", size:"5", text: "Something else"},
-            {_id: "678", widgetType : "YOUTUBE", pageId: "321", width:"90%", url: "https://www.youtube.com/embed/vlDzYIIOYmM"},
+            {_id: "678", widgetType : "YOUTUBE", pageId: "321", width:"75%", url: "https://www.youtube.com/embed/vlDzYIIOYmM"},
             {_id: "789", widgetType : "HTML", pageId: "321", text: "<p>Lorem <i>Ipsum</i> something</p>"}
         ];
 
@@ -23,7 +23,36 @@
         };
 
         return api;
+
         function createWidget(pageId, widget) {
+            var wgid = (parseInt(widgets[widgets.length -1]._id) + 1).toString();
+            var newWidget;
+            switch (widget.type){
+                case "HEADER":
+                    newWidget = {   _id: wgid,
+                                    widgetType: widget.type,
+                                    pageId: pageId,
+                                    size: widget.size,
+                                    text: widget.text};
+                    break;
+                case "IMAGE":
+                    newWidget = {   _id: wgid,
+                                    widgetType: widget.type,
+                                    pageId: pageId,
+                                    width: widget.width,
+                                    url: widget.url};
+                    break;
+                case "YOUTUBE":
+                    newWidget = {   _id: wgid,
+                                    widgetType: widget.type,
+                                    pageId: pageId,
+                                    width: widget.width,
+                                    url: widget.url};
+                    break;
+            }
+
+            widgets.push(newWidget);
+            return angular.copy(newWidget);
         }
         function findWidgetByPageId(pid) {
             var widgetsList = [];
--- public/assignment/views/widget/widget.controller.client.js
+++ public/assignment/views/widget/widget.controller.client.js
@@ -52,27 +52,72 @@
     
     function NewWidgetController($routeParams, $location, WidgetService) {
         var vm = this;
+        vm.uid = $routeParams.uid;
+        vm.wid = $routeParams.wid;
+        vm.pid = $routeParams.pid;
+
         vm.navigateToProfile = navigateToProfile;
         vm.navigateToWidgets = navigateToWidgets;
+        vm.createHeaderWidget = createHeaderWidget;
+        vm.createImageWidget = createImageWidget;
+        vm.createYoutubeWidget = createYoutubeWidget;
+
         function navigateToProfile() {
-            $location.url("user/"+$routeParams["uid"]);
+            $location.url("user/"+vm.uid);
         }
         function navigateToWidgets() {
-            $location.url("user/"+$routeParams["uid"]+"/website/"+$routeParams["wid"]+"/page/"+$routeParams["pid"]+"/widget");
+            $location.url("user/"+vm.uid+"/website/"+vm.wid+"/page/"+vm.pid+"/widget");
+        }
+
+        function createHeaderWidget(headerSize) {
+            var widget = {type: "HEADER",
+                          size: headerSize,
+                          text: "Sample Heading "+headerSize}
+            var newWidget = WidgetService.createWidget(vm.pid, widget);
+            if(newWidget != null){
+                $location.url("/user/"+vm.uid+"/website/"+vm.wid+"/page/"+vm.pid+"/widget/"+newWidget._id);
+            }
+        }
+
+        function createImageWidget() {
+            var widget = {type: "IMAGE",
+                width: "100%",
+                url: "https://www.djaysgourmet.com.au/wp-content/uploads/2016/02/sample.jpg"}
+            var newWidget = WidgetService.createWidget(vm.pid, widget);
+            if(newWidget != null){
+                $location.url("/user/"+vm.uid+"/website/"+vm.wid+"/page/"+vm.pid+"/widget/"+newWidget._id);
+            }
+        }
+        function createYoutubeWidget() {
+            var widget = {type: "YOUTUBE",
+                width: "100%",
+                url: "https://www.youtube.com/embed/vlDzYIIOYmM"}
+            var newWidget = WidgetService.createWidget(vm.pid, widget);
+            if(newWidget != null){
+                $location.url("/user/"+vm.uid+"/website/"+vm.wid+"/page/"+vm.pid+"/widget/"+newWidget._id);
+            }
         }
     }
     
     function EditWidgetController($routeParams, $location, WidgetService) {
         var vm = this;
-        vm.widgetId = $routeParams["wgid"];
-        vm.widget = WidgetService.findWidgetById(vm.widgetId);
+        vm.uid = $routeParams.uid;
+        vm.wid = $routeParams.wid;
+        vm.pid = $routeParams.pid;
+        vm.wgid = $routeParams.wgid;
+
+        function init() {
+            vm.widget = WidgetService.findWidgetById(vm.wgid);
+        }
+        init();
+
         vm.navigateToProfile = navigateToProfile;
         vm.navigateToWidgets = navigateToWidgets;
         function navigateToProfile() {
-            $location.url("user/"+$routeParams["uid"]);
+            $location.url("user/"+vm.uid);
         }
         function navigateToWidgets() {
-            $location.url("user/"+$routeParams["uid"]+"/website/"+$routeParams["wid"]+"/page/"+$routeParams["pid"]+"/widget");
+            $location.url("user/"+vm.uid+"/website/"+vm.wid+"/page/"+vm.pid+"/widget");
         }
     }
 })();
