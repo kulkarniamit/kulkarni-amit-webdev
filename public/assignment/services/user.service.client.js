46151c5398ea3ff3339042681b5088385173bd14
Amit Kulkarni <kulkarni.am@husky.neu.edu>
2017-02-20 12:10:46 -0500

    Moved sensitive data from client to server

--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -4,12 +4,12 @@
         .factory("UserService",UserService);
 
     function UserService($http) {
-        var users = [
-            {_id: "123", username : "alice", password : "alice", email:"alice@gmail.com", firstName: "Alice", lastName: "Wonder"},
-            {_id: "234", username : "bob", password : "bob", email:"bob@gmail.com", firstName: "Bob", lastName: "Marley"},
-            {_id: "345", username : "charly", password : "charly", email:"charly@gmail.com", firstName: "Charly", lastName: "Garcia"},
-            {_id: "456", username : "jannunzi", password : "jannunzi", email:"jannunaziato@gmail.com", firstName: "Jose", lastName: "Annunzi"}
-        ];
+        // var users = [
+        //     {_id: "123", username : "alice", password : "alice", email:"alice@gmail.com", firstName: "Alice", lastName: "Wonder"},
+        //     {_id: "234", username : "bob", password : "bob", email:"bob@gmail.com", firstName: "Bob", lastName: "Marley"},
+        //     {_id: "345", username : "charly", password : "charly", email:"charly@gmail.com", firstName: "Charly", lastName: "Garcia"},
+        //     {_id: "456", username : "jannunzi", password : "jannunzi", email:"jannunaziato@gmail.com", firstName: "Jose", lastName: "Annunzi"}
+        // ];
 
         var api={
             "createUser":createUser,
--- public/assignment/services/website.service.client.js
+++ public/assignment/services/website.service.client.js
@@ -4,14 +4,14 @@
         .factory("WebsiteService",WebsiteService);
 
     function WebsiteService($http) {
-        var websites = [
-            {_id: "123", name : "Facebook", developerId: "123", description:"Connect with friends and the world around you on Facebook."},
-            {_id: "234", name : "Twitter", developerId: "123", description:"Our mission: To give everyone the power to create and share ideas and information instantly, without barriers."},
-            {_id: "345", name : "Gizmodo", developerId: "111", description:"Gizmodo is a design, technology, science and science fiction website that often writes articles on politics. It was originally launched as part of the Gawker Media"},
-            {_id: "456", name : "Tic Tac Toe", developerId: "111", description:"Tic-tac-toe is a paper-and-pencil game for two players, X and O, who take turns marking the spaces in a 3×3 grid."},
-            {_id: "567", name : "Checkers", developerId: "333", description:"Checkers is a group of strategy board games for two players which involve diagonal moves of uniform game pieces and mandatory captures by jumping over opponent pieces. Draughts developed from alquerque."},
-            {_id: "678", name : "Chess", developerId: "123", description:"Play chess on Chess.com - the #1 chess community with millions of members around the world. Fun stats, analysis, and training tools for players of all levels."}
-        ];
+        // var websites = [
+        //     {_id: "123", name : "Facebook", developerId: "123", description:"Connect with friends and the world around you on Facebook."},
+        //     {_id: "234", name : "Twitter", developerId: "123", description:"Our mission: To give everyone the power to create and share ideas and information instantly, without barriers."},
+        //     {_id: "345", name : "Gizmodo", developerId: "111", description:"Gizmodo is a design, technology, science and science fiction website that often writes articles on politics. It was originally launched as part of the Gawker Media"},
+        //     {_id: "456", name : "Tic Tac Toe", developerId: "111", description:"Tic-tac-toe is a paper-and-pencil game for two players, X and O, who take turns marking the spaces in a 3×3 grid."},
+        //     {_id: "567", name : "Checkers", developerId: "333", description:"Checkers is a group of strategy board games for two players which involve diagonal moves of uniform game pieces and mandatory captures by jumping over opponent pieces. Draughts developed from alquerque."},
+        //     {_id: "678", name : "Chess", developerId: "123", description:"Play chess on Chess.com - the #1 chess community with millions of members around the world. Fun stats, analysis, and training tools for players of all levels."}
+        // ];
 
         var api={
             "createWebsite":createWebsite,
