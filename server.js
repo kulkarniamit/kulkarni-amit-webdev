cb9282e228cefd83786d0b9ea0aedbacda606488
Amit Kulkarni <kulkarni.am@husky.neu.edu>
2017-03-28 12:53:02 -0400

    Starting assignment 6

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Passport configuration
+var passport      = require('passport');
+var cookieParser  = require('cookie-parser');
+var session       = require('express-session');
+
+app.use(session({
+    secret: 'this is the secret', //Store it in process.env.SESSION_SECRET
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
